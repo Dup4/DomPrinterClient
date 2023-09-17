d69867b682a2d05144c18322b308542f2c9598da
Dup4 <lyuzhi.pan@gmail.com>
2023-09-17 16:27:30 +0800

    chore: dev
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- main.go
+++ main.go
@@ -17,27 +17,38 @@
 package main
 
 import (
-	"Hertz/hertz_gen/domprinter"
-	"Hertz/hz_client/domprinter_service"
+	// "Hertz/hertz_gen/domprinter"
+	// "Hertz/hz_client/domprinter_service"
 	"context"
 	"fmt"
+
+	"github.com/Dup4/domprinter/hertz_client/domprinter"
+	"github.com/Dup4/domprinter/hertz_client/domprinter_service"
 )
 
-const hostname = "http://172.22.237.2:8881"
+// const hostname = "http://172.22.237.2:8881"
+const hostname = "http://127.0.0.1:8888"
 
 func main() {
 	//nowclient = domprinter_service.DefaultClient
 	//res,rsp,err := domprinter_service.FetchPrintTask(context.Background(),,"POST")
-	var defaultClient, _ = domprinter_service.NewDOMPrinterServiceClient(hostname)
+	defaultClient, err := domprinter_service.NewDOMPrinterServiceClient(hostname)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	var task domprinter.FetchPrintTaskReq
+	// var task domprinter.FetchPrintTaskReq
+	task := domprinter.NewFetchPrintTaskReq()
 	task.TaskState = domprinter.TaskStateEnum_Submitted
 	task.BaseReq = domprinter.NewBaseReq()
+	task.BaseReq.AuthToken = ""
 
-	fmt.Println("namo1")
-	rsp, res, err := defaultClient.FetchPrintTask(context.Background(), &task)
+	rsp, res, err := defaultClient.FetchPrintTask(context.Background(), task)
 	if err != nil {
 		fmt.Println("err Client", err)
+		return
 	}
+
 	fmt.Println("namomo", rsp, res)
 }
